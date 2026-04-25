3b021d22ce03768d9fc522df1319889045f15ee7
CharcoalFeather <schoolacc1280@gmail.com>
2026-04-25 09:07:14 -0500

    Add psuedocode for game planning

---
+++ src/project.py
@@ -0,0 +1,17 @@
+#Psuedocode for A Unicorn's Wander
+
+#imports
+
+#Gameloop
+
+#Global Variables
+
+#Player Class
+
+#Background 
+
+#Main function
+
+
+#if __name__ =="__main__":
+    #main(window)
