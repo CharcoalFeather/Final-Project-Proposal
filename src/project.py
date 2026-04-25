11e6b777f343d8d60c10806f65617243627493db
CharcoalFeather <schoolacc1280@gmail.com>
2026-04-25 09:11:42 -0500

    Add main game loop for running game

--- src/project.py
+++ src/project.py
@@ -1,17 +1,41 @@
-#Psuedocode for A Unicorn's Wander
-
-#imports
+import os 
+import random
+import math 
+import pygame
 
 #Gameloop
+pygame.init()
+pygame.display.set_caption("A Unicorn's Wander")
 
 #Global Variables
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 600, 600
+FPS = 60 
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #Player Class
 
 #Background 
 
-#Main function
+def main(window):
+    clock = pygame.time.Clock()    
+    run = True 
+
+    while run:
+        clock.tick(FPS)
+    #This while loop ensure our game runs at 60 fps.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break 
+
+    pygame.quit()
+    quit()
 
 
-#if __name__ =="__main__":
-    #main(window)
+    pass
+    #Inside of main will be our event loop. 
+
+
+if __name__ =="__main__":
+    main(window)
