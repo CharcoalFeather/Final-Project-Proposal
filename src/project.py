ca7948f7d6b96714a56318f39b2d56834d2e4dce
CharcoalFeather <schoolacc1280@gmail.com>
2026-05-03 14:44:41 -0500

    Add Sprite Animation and Terrain Test

--- src/project.py
+++ src/project.py
@@ -42,14 +42,23 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     
     return all_sprites
 
+def get_block(size):
+    path = join("assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
 
 class Player(pygame.sprite.Sprite):
     #using sprite allows us to do pixel perfect collision
     COLOR = (255, 0, 0) #class variable so its accesible 
     GRAVITY = 1 
     SPRITES = load_sprite_sheets("MainCharacters", "Unicorn", 32, 32, True)
+    ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height) # rect is a tuple storing values
         #We will move our player using a velocity in a direction
         self.x_vel = 0 
@@ -74,6 +83,7 @@ class Player(pygame.sprite.Sprite):
         if self.direction != "right":
             self.direction = "right"
             self.animation_count = 0
+            self.update()
     
     def loop(self, fps):
         #we call the loop once every frame, and it will move the character.
@@ -81,10 +91,47 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1 
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = self.animation_count // self.ANIMATION_DELAY % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+
 
     def draw(self, win):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+         super().__init__()
+         self.rect = pygame.Rect(x, y, width, height)
+         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+         self.width = width
+         self.height = height
+         self.name = name 
+    
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 def get_background(name):
@@ -103,10 +150,13 @@ def get_background(name):
     return tiles, image
 
 
-def draw(window, background, bg_image, player):
+def draw(window, background, bg_image, player, objects):
 # We are looping through tiles and they will return determined by previously mentioned code.
     for tile in background:
         window.blit(bg_image, tile)
+
+    for obj in objects: 
+        obj.draw(window)
     
     player.draw(window)
 
@@ -127,7 +177,9 @@ def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Pink.png")
     
+    block_size = 96
     player = Player(100, 100, 50, 50)
+    blocks = [Block(0, HEIGHT - block_size, block_size)]
 
     run = True 
 
@@ -141,7 +193,7 @@ def main(window):
     
         player.loop(FPS)
         handle_move(player)
-        draw(window, background, bg_image, player)
+        draw(window, background, bg_image, player, blocks)
 
     pygame.quit()
     quit()
