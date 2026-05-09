f173a6e7760e3aa53bedf0f6a95b6c8555eb0605
CharcoalFeather <schoolacc1280@gmail.com>
2026-05-09 07:27:42 -0500

    Development Restart & Clean up

--- src/project.py
+++ src/project.py
@@ -1,243 +1,81 @@
-import os 
-import random
-import math
 import pygame
-from os import listdir
-from os.path import isfile, join
-from pygame import mixer
+from pygame.locals import * 
+
 pygame.init()
 
+WIDTH = 600
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("A Unicorn's Wander")
 
-#Major Cleanup is needed. To Do: 
-#Turn the background into a picture of the mountains.
-#Implement a ground of blocks
-#Ensure gravity is sound
-#Ensure Sprite is sound. 
-#Apply Music
+tile_size = 60
 
-BG_COLOR = (255, 255, 255)
-WIDTH, HEIGHT = 600, 600
-FPS = 60 
-PLAYER_VEL = 5
-
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-
-def flip(sprites):
-    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
-
-def load_sprite_sheets(dir1, dir2, width, height, direction=False):
-    path = join("assets", dir1, dir2)
-    images = [f for f in listdir(path) if isfile(join(path, f))]
-
-    all_sprites = {}
-
-    for image in images:
-        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
-
-        sprites = [] 
-        for i in range(sprite_sheet.get_width() // width):
-            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
-            rect = pygame.Rect(i * width, 0, width, height)
-            surface.blit(sprite_sheet, (0, 0), rect)
-            sprites.append(pygame.transform.scale2x(surface))
-    
-        if direction: 
-            all_sprites[image.replace(".png", "") + "_right"] = sprites
-            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
-        else: 
-            all_sprites[image.replace(".png", "")] = sprites
-    
-    return all_sprites
-
-def get_block(size):
-    path = join("assets", "Terrain", "Terrain.png")
-    image = pygame.image.load(path).convert_alpha()
-    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
-    rect = pygame.Rect(96, 0, size, size)
-    surface.blit(image, (0, 0), rect)
-    return pygame.transform.scale2x(surface)
-
-class Player(pygame.sprite.Sprite):
-    #using sprite allows us to do pixel perfect collision
-    COLOR = (255, 0, 0) #class variable so its accesible 
-    GRAVITY = 1 
-    SPRITES = load_sprite_sheets("MainCharacters", "Unicorn", 32, 32, True)
-    ANIMATION_DELAY = 3
-
-    def __init__(self, x, y, width, height):
-        super().__init__()
-        self.rect = pygame.Rect(x, y, width, height) # rect is a tuple storing values
-        #We will move our player using a velocity in a direction
-        self.x_vel = 0 
-        self.y_vel = 0 
-        self.mask = None
-        self.direction = "left"
-        self.animation_count = 0
-        self.fall_count = 0
-   
-    def move(self, dx, dy):
-        self.rect.x += dx
-        self.rect.y += dy
-
-    def move_left(self, vel):
-        self.x_vel = -vel
-        if self.direction != "left":
-            self.direction = "left"
-            self.animation_count = 0
-    
-    def move_right(self, vel):
-        self.x_vel = vel
-        if self.direction != "right":
-            self.direction = "right"
-            self.animation_count = 0
-            self.update()
-    
-    def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
-        self.move(self.x_vel, self.y_vel)
-
-        self.fall_count += 1 
-        self.update_sprite()
-
-    def landed(self):
-        self.fall_count = 0
-        self.y_vel = 0
-
-    def update_sprite(self):
-        sprite_sheet = "idle"
-        if self.x_vel != 0:
-            sprite_sheet = "run"
-
-        sprite_sheet_name = sprite_sheet + "_" + self.direction
-        sprites = self.SPRITES[sprite_sheet_name]
-        sprite_index = self.animation_count // self.ANIMATION_DELAY % len(sprites)
-        self.sprite = sprites[sprite_index]
-        self.animation_count += 1
-
-    def update(self):
-        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
-        self.mask = pygame.mask.from_surface(self.sprite)
+#Load images
+Background = pygame.image.load('assets/Background/Mountains.png')
+grass_img = pygame.image.load('assets/Terrain/grass.png')
 
 
-    def draw(self, win, offset_x):
-        win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #load images
+        dirt_img = pygame.image.load('assets/Terrain/dirt.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world_data = [
+[0, 0, 0, 0, 0],
+[0, 0, 0, 2, 2],
+[0, 2, 0, 0, 0],
+[0, 0, 0, 0, 0],
+[1, 2, 2, 0, 0],
+[0, 0, 0, 0, 0],
+[0, 0, 0, 1, 1],
+[0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0],
+[1, 2, 2, 2, 1, 1, 2, 2, 2, 1]
+]
+
+world = World(world_data)
 
 
-class Object(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, name=None):
-         super().__init__()
-         self.rect = pygame.Rect(x, y, width, height)
-         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
-         self.width = width
-         self.height = height
-         self.name = name 
-    
-    def draw(self, win, offset_x):
-        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
+run = True
+while run:
 
-class Block(Object):
-    def __init__(self, x, y, size):
-        super().__init__(x, y, size, size)
-        block = get_block(size)
-        self.image.blit(block, (0, 0))
-        self.mask = pygame.mask.from_surface(self.image)
+    screen.blit(Background, (0,0))
+    world.draw()
+ 
 
-
-def get_background(name):
-    path = join("assets", "Background", name)
-    image = pygame.image.load(path).convert()
-    return pygame.transform.scale(image, (WIDTH, HEIGHT))
-
-def draw(window, bg_image, player, objects, offset_x):
-    window.blit(bg_image, (0, 0))
-
-    for obj in objects:
-        obj.draw(window, offset_x)
-
-    player.draw(window, offset_x)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
-
-def handle_vertical_collision(player, objects, dy):
-    collided_objects = []
-    for obj in objects:
-        if pygame.sprite.collide_mask(player, obj):
-            if dy > 0:
-                player.rect.bottom = obj.rect.top
-                player.landed()
-            elif dy < 0:
-                player.rect.top = obj.rect.bottom
-                player.hit_head()
-
-            collided_objects.append(obj)
-
-    return collided_objects
-
-def collide(player, objects, dx):
-    player.move(dx, 0)
-    player.update()
-    collided_object = None
-    for obj in objects:
-        if pygame.sprite.collide_mask(player, obj):
-            collided_object = obj
-            break
-
-    player.move(-dx, 0)
-    player.update()
-    return collided_object
-
-def sound(sound):
-    # Sound
-    path = join("assets", "background.mp3")
-    mixer.music.load("background.mp3")
-    mixer.music.play(-1)
-
-def handle_move(player, objects):
-    keys = pygame.key.get_pressed()
-
-    player.x_vel = 0
-    collide_left = collide(player, objects, -PLAYER_VEL * 2)
-    collide_right = collide(player, objects, PLAYER_VEL * 2)
-
-    if keys[pygame.K_LEFT] and not collide_left:
-        player.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT] and not collide_right:
-        player.move_right(PLAYER_VEL)
-
-    vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
-    to_check = [collide_left, collide_right, *vertical_collide]
-
-
-def main(window):
-    window.fill((0, 0, 0))
-    bg_image = get_background("Mountains.png")
-    clock = pygame.time.Clock()
-    block_size = 96
-
-    player = Player(100, 100, 50, 50)
-    floor = [Block(i * block_size, HEIGHT - block_size, block_size)
-             for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
-    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size)]
-    offset_x = 0
-    scroll_area_width = 200
-
-    run = True
-    while run:
-        clock.tick(FPS)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
-
-        player.loop(FPS)
-        handle_move(player, objects)
-        draw(window, bg_image, player, objects, offset_x)
-
-    pygame.quit()
-    quit()
-
-if __name__ =="__main__":
-    main(window)
+pygame.quit()
